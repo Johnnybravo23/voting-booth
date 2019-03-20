8adac3787cd2258ffca3526b4455ae4435cfebbb
Johnnybravo23 <johnnyjohannes.jr@yahoo.com>
2019-03-20 08:42:35 -0700

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: 'mongodb://johnnybravo:abc123@ds117866.mlab.com:17866/pusherpoll',
+  pusherAppId: '739969',
+  pusherKey: '5738d159d687e133e336',
+  pusherSecret: '55ee939f4cbaf9b9edd4',
+  pusherCluster: 'ap2',
+  pusherEncrypted: true
+};
